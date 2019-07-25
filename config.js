bff1ada5b4da5c9b5e7aa2275a9b6e11c044f6f0
mras2303 <marek.kowalonek@gmail.com>
2019-07-25 16:44:03 +0200

    Add support for environment variable

--- config.js
+++ config.js
@@ -13,7 +13,8 @@ const setHeaders = res => {
 
 const mongo_connect = () => {
     mongoose.set('useCreateIndex', true);
-    mongoose.connect(process.env.MY_KEY, { useNewUrlParser: true })
+    mongoose.connect(process.env.mongo_url, { useNewUrlParser: true })
+
 }
 
 const useMiddleware = app => {
