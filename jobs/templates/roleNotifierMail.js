52502bf0daefb3105b473e0ce313a28de8fa2c71
antekpapieski <maciejpapiewski9@gmail.com>
2019-07-26 19:36:45 +0200

    removed obsolete # character from mail template for roles notification

--- jobs/templates/roleNotifierMail.js
+++ jobs/templates/roleNotifierMail.js
@@ -1,6 +1,6 @@
 export default (roles) => {
     let messageBody = "";
-    roles.forEach( (role) => messageBody += `--> role "${role.name}" of color #${role.color} is inactive\n`);
+    roles.forEach( (role) => messageBody += `--> role "${role.name}" of color ${role.color} is inactive\n`);
     return `
 There are new roles that need to be reviewed and activated!
 ${messageBody}
