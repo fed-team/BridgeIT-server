f6bcdce578b318ffacd1008c8e722632bf9200a9
hendrysiak <lukasz.hendrysiak@gmail.com>
2019-08-06 21:50:52 +0200

    #1ad6tb Fixed delete method

--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,30 @@
-import expressPromiseRouter from 'express-promise-router';
+import expressPromiseRouter from "express-promise-router";
 
-import {
-    user as Controller
-} from '../controllers';
-import {
-    validateParam,
-    validateBody,
-    schema
-} from '../helpers/routeHelpers';
+import { user as Controller } from "../controllers";
+import { validateParam, validateBody, schema } from "../helpers/routeHelpers";
 
 const router = expressPromiseRouter();
 
-router.route('/')
-    .get(Controller.index)
-    .post(validateBody(schema.user.post), Controller.add);
+router
+  .route("/")
+  .get(Controller.index)
+  .post(validateBody(schema.user.post), Controller.add);
 
-router.route('/:desiredAction((((de)?)activate))/:id')
-    .patch(validateParam(schema.id, 'id'), Controller.switchActivity);
+router
+  .route("/:desiredAction((((de)?)activate))/:id")
+  .patch(validateParam(schema.id, "id"), Controller.switchActivity);
 
-router.route('/:id')
-    .get(validateParam(schema.id, 'id'), Controller.get)
-    .put([validateParam(schema.id, 'id'), validateBody(schema.user.put)], Controller.replace)
-    .patch([validateParam(schema.id, 'id'), validateBody(schema.user.patch)], Controller.update);
-
-router.route('/delete/:id')
-    .post(validateBody(schema.user.post), Controller.deleteUser)
+router
+  .route("/:id")
+  .get(validateParam(schema.id, "id"), Controller.get)
+  .put(
+    [validateParam(schema.id, "id"), validateBody(schema.user.put)],
+    Controller.replace
+  )
+  .patch(
+    [validateParam(schema.id, "id"), validateBody(schema.user.patch)],
+    Controller.update
+  )
+  .delete(validateParam(schema.id, "id"), Controller.deleteUser);
 
 export default router;
